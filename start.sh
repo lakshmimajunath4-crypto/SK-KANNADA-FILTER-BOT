2abb44726de299023e97556deafeb2bdc2d7f690
lakshmimajunath4-crypto <lakshmimajunath4@gmail.com>
2026-01-22 22:05:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/JisshuTG/Jisshu-filter-bot /Jisshu-filter-bot
+  git clone https://github.com/surya/Surya-filter-bot /Surya-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Jisshu-filter-bot
+  git clone $UPSTREAM_REPO /Surys-filter-bot
 fi
 cd /Jisshu-filter-bot
 pip3 install -U -r requirements.txt
-echo "Starting Jisshu filter bot...."
+echo "Starting Surya filter bot...."
 python3 bot.py
